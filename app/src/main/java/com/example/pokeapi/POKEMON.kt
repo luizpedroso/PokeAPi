a4f32239e0ec11f60e5d390fb8d3c1a58813c7d2
luizpedroso <luizcarlospedrosojr@gmail.com>
2019-08-16 10:55:25 -0300

    correcao no calculo de altura

--- app/src/main/java/com/example/pokeapi/POKEMON.kt
+++ app/src/main/java/com/example/pokeapi/POKEMON.kt
@@ -3,12 +3,11 @@ package com.example.pokeapi
 class POKEMON {
 
     var id: String? = null
-    var name: String? = null
-    var weight: String? = null
-    var height: String? = null
+    var weight: Double? = null
+    var height: Double? = null
 
     override fun toString(): String {
-        return ("Altura: ${height}m   \nPeso: ${weight}kg")
+        return ("Altura:  ${height?.div(100)}m\nPeso: ${weight?.div(100)}kg")
     }
 
 }
